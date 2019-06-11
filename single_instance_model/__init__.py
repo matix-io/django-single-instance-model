2c192615617f8430121c88b97ee48fb82161eca0
Connor Bode <connor.bode@gmail.com>
2019-06-11 18:32:42 -0400

    v1.0.6

--- single_instance_model/__init__.py
+++ single_instance_model/__init__.py
@@ -1,11 +1,24 @@
 from django.dispatch import receiver
 from django.db.backends.signals import connection_created
+from django.db.migrations.executor import MigrationExecutor
+from django.db import connections, DEFAULT_DB_ALIAS
 from django.apps import apps
 from .models import SingleInstanceModel
 
 
+def is_database_synchronized(database):
+    connection = connections[database]
+    connection.prepare_database()
+    executor = MigrationExecutor(connection)
+    targets = executor.loader.graph.leaf_nodes()
+    return False if executor.migration_plan(targets) else True
+
+
 @receiver(connection_created)
 def my_receiver(connection, **kwargs):
+	if not is_database_synchronized(DEFAULT_DB_ALIAS):
+		return
+	
 	with connection.cursor() as cursor:
 		for key, app in apps.all_models.items():
 			for k2, model in app.items():
